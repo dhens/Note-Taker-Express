a09af5b23ce7c111b20a6853b8a1045ce9a5363d
dhens <dawsonhensel@gmail.com>
2020-02-28 14:23:26 -0800

    added save note post functionality.

--- server.js
+++ server.js
@@ -1,26 +1,51 @@
 const express = require('express');
+const uniqid = require('uniqid');
 const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + '/public/'));
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-app.get('/notes', function(req, res) {
+app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 })
 
-app.get('/api/notes', function(req, res) {
+app.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/db/db.json'));
 });
 
-app.listen(port, function() {
+app.post('/api/notes', (req, res) => {
+    req.body.id = uniqid();
+    const newNote = req.body;
+    
+    fs.readFile(path.join(__dirname + '/db/db.json'), (err, data) => {
+        // Error is thrown if you delete all notes then try to set savedNotes equal to db.json data 
+        // This handles an empty db.json file
+        let savedNotes;
+        try {
+            savedNotes = JSON.parse(data);
+        }
+        catch {
+            savedNotes = []
+        }
+        savedNotes.push(newNote);
+
+        fs.writeFile(__dirname + '/db/db.json', JSON.stringify(savedNotes), err => {
+            if (err) throw err;
+            console.log('Added new note to db.json')
+
+        });
+    });
+});
+
+app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
