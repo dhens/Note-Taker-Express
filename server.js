4362daf027a72e14c08848fdb90d6d45f1cc01b0
dhens <dawsonhensel@gmail.com>
2020-02-28 14:36:19 -0800

    added readDatabase function and initial note delete functionality

--- server.js
+++ server.js
@@ -25,26 +25,34 @@ app.get('/api/notes', (req, res) => {
 app.post('/api/notes', (req, res) => {
     req.body.id = uniqid();
     const newNote = req.body;
-    
-    fs.readFile(path.join(__dirname + '/db/db.json'), (err, data) => {
-        // Error is thrown if you delete all notes then try to set savedNotes equal to db.json data 
-        // This handles an empty db.json file
-        let savedNotes;
-        try {
-            savedNotes = JSON.parse(data);
-        }
-        catch {
-            savedNotes = []
-        }
-        savedNotes.push(newNote);
+    const data = readDatabase();
+    // Error is thrown if you delete all notes then try to set savedNotes equal to db.json data 
+    // This handles an empty db.json file
+    let savedNotes;
+    try {
+        savedNotes = JSON.parse(data);
+    }
+    catch {
+        savedNotes = []
+    }
+    savedNotes.push(newNote);
 
-        fs.writeFile(__dirname + '/db/db.json', JSON.stringify(savedNotes), err => {
-            if (err) throw err;
-            console.log('Added new note to db.json')
+    fs.writeFile(__dirname + '/db/db.json', JSON.stringify(savedNotes), err => {
+        if (err) throw err;
+        console.log('Added new note to db.json')
 
-        });
     });
 });
+
+app.delete('/api/notes/:id', (req, res) => {
+    const data = readDatabase();
+});
+
+function readDatabase() {
+    fs.readFile(path.join(__dirname + '/db/db.json'), (err, data) => {
+        return data;
+    });
+}
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
