3b9b9f06939c65194e951f933dc8919592c9a360
Dawson Hensel <dawsonhensel@gmail.com>
2020-03-15 14:39:01 -0700

    updated PORT to be dynamic. either sets to process.env or 5000

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
